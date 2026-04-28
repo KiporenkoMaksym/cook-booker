8cac84d2a034fb416179ce07955ae51d6ad914c6
Max <kiporenkomaks8@gmail.com>
2026-04-28 20:23:38 +0300

    add base.html, index.html, sidebar.html, styles.css; in settings.py wrote settings to static files; connected bootstrap 5 and styles.css

--- classes/views.py
+++ classes/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
 
 
-def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello, world. You're the main page.")
+def index(request):
+    return render (request, "classes/index.html")
