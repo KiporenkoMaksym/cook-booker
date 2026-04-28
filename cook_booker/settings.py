424a9b58bb1cb498062030db03fcfdeb72c3d3a7
Max <kiporenkomaks8@gmail.com>
2026-04-28 21:06:32 +0300

    enabled django debug toolbars and added pagination.html

--- cook_booker/settings.py
+++ cook_booker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
     "classes",
 ]
 
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "cook_booker.urls"
@@ -127,3 +129,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "classes.Chef"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
