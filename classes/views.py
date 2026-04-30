5168d16d4c199fe6cdb0dbb36d413a5edc4ed760
Max <kiporenkomaks8@gmail.com>
2026-04-30 20:59:20 +0300

    added the number of chefs, cuisines, ingredients, courses, as well as the number of entries to the page to the homepage

--- classes/views.py
+++ classes/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 
+from classes.models import Chef, Ingredient, Cuisine, CookingClass
+
 
 def index(request):
-    return render (request, "classes/index.html")
+
+    num_chefs = Chef.objects.count()
+    num_cuisines = Cuisine.objects.count()
+    num_ingredients = Ingredient.objects.count()
+    num_cooking_classes = CookingClass.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_chefs": num_chefs,
+        "num_cuisines": num_cuisines,
+        "num_ingredients": num_ingredients,
+        "num_cooking_classes": num_cooking_classes,
+        "num_visits": num_visits + 1,
+    }
+
+    return render (request, "classes/index.html", context)
