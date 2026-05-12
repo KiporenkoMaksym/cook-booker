19f7163e65b73f150834d726747efd7fe9a01270
Max <kiporenkomaks8@gmail.com>
2026-05-12 22:29:28 +0300

    In settings.py I changed DIRS, added LOGIN_REDIRECT_URL = "classes:index". Added login.html and created a login path in urls.

--- cook_booker/urls.py
+++ cook_booker/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
     path("", include("classes.urls", namespace="classes")),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
